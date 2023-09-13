3b960a8a0b2e21536ea64124aee129c4a4651cd6
Rachwan <rachwan.harb2023@gmail.com>
2023-09-13 18:43:01 +0300

    Fix the the main placement in HTML and Added 100vh height for the div with root id

--- src/App.js
+++ src/App.js
@@ -26,60 +26,60 @@ class App extends Component {
   render() {
     return (
       <div className="app">
-        <main role="main" class="main">
         <header className="header">
           <nav className="nav">
               <input type="text" name="" id="" placeholder="Type in a city here"/>
               <button className="button">FIND WEATHER</button>
           </nav>
         </header>
-        <section class="w-details">
-          <div className="content">
-            <div className="w-image">
-              <img src={partlycloudy} alt="Partly Cloudy" />
-              <p className="desc">overcast clouds</p>
+        <main role="main" class="main">
+          <section class="w-details">
+            <div className="content">
+              <div className="w-image">
+                <img src={partlycloudy} alt="Partly Cloudy" />
+                <p className="desc">overcast clouds</p>
+              </div>
+              <p className="temp">Temperature <span>10&deg;C to 11&deg;C</span></p>
+              <p className="details"><span>Humidity</span> 78% <span>Pressure</span> 1008.48</p>
             </div>
-            <p className="temp">Temperature <span>10&deg;C to 11&deg;C</span></p>
-            <p className="details"><span>Humidity</span> 78% <span>Pressure</span> 1008.48</p>
-          </div>
-        </section>
-        <section className="w-hour">  
-          <div>
-              <p className="time">03:00</p>
+          </section>
+          <section className="w-hour">  
+            <div>
+                <p className="time">03:00</p>
+                <img src={partlycloudy} alt="Partly Cloudy" />
+                <p className="temp">8&deg;C</p>
+            </div>
+            <div>
+              <p className="time">06:00</p>
               <img src={partlycloudy} alt="Partly Cloudy" />
-              <p className="temp">8&deg;C</p>
-          </div>
-          <div>
-            <p className="time">06:00</p>
-            <img src={partlycloudy} alt="Partly Cloudy" />
-            <p className="temp">9&deg;C</p>
-          </div>
-          <div>
-            <p className="time">09:00</p>
-            <img src={clear} alt="Clear Cloudy" />
-            <p className="temp">14&deg;C</p>
-          </div>
-          <div>
-            <p className="time">12:00</p>
-            <img src={clear} alt="Clear Cloudy" />
-            <p className="temp">17&deg;C</p>
-          </div>
-          <div>
-            <p className="time">15:00</p>
-            <img src={clear} alt="Clear Cloudy" />
-            <p className="temp">18&deg;C</p>
-          </div>
-          <div>
-            <p className="time">18:00</p>
-            <img src={clear} alt="Clear Cloudy" />
-            <p className="temp">16&deg;C</p>
-          </div>
-          <div>
-            <p className="time">21:00</p>
-            <img src={partlycloudy} alt="Partly Cloudy" />
-            <p className="temp">13&deg;C</p>
-          </div>
-        </section>
+              <p className="temp">9&deg;C</p>
+            </div>
+            <div>
+              <p className="time">09:00</p>
+              <img src={clear} alt="Clear Cloudy" />
+              <p className="temp">14&deg;C</p>
+            </div>
+            <div>
+              <p className="time">12:00</p>
+              <img src={clear} alt="Clear Cloudy" />
+              <p className="temp">17&deg;C</p>
+            </div>
+            <div>
+              <p className="time">15:00</p>
+              <img src={clear} alt="Clear Cloudy" />
+              <p className="temp">18&deg;C</p>
+            </div>
+            <div>
+              <p className="time">18:00</p>
+              <img src={clear} alt="Clear Cloudy" />
+              <p className="temp">16&deg;C</p>
+            </div>
+            <div>
+              <p className="time">21:00</p>
+              <img src={partlycloudy} alt="Partly Cloudy" />
+              <p className="temp">13&deg;C</p>
+            </div>
+          </section>
         </main>
       </div>
     );
