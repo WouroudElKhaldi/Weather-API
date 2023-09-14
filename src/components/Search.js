cefd2f0d289b3c298d0956362f14a878d2ede0bd
Rachwan <rachwan.harb2023@gmail.com>
2023-09-14 21:59:04 +0300

    Save the name of the city inside the input in a variable

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 const Search = () => {
+  const [city, setCity] = useState("");
+
+  const handleInputChange = (e) => {
+    setCity(e.target.value);
+  }
+
+  const handleFindWeather = () => {
+    console.log("Searching weather for city:", city); // Print the city in the console  
+  } 
+
   return (
       <nav className="nav">
-        <input type="text" name="" id="" placeholder="Type in a city here"/>
-        <button className="button">FIND WEATHER</button>
+        <input type="text" placeholder="Type in a city here" value={city} onChange={handleInputChange}/>
+        <button className="button" onClick={handleFindWeather}>FIND WEATHER</button>
       </nav>
   );
 }
